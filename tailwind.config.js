b0beba78be6b555f9d6866f172c6cb1e37a4a5f0
Chan-con <goa.kondo@gmail.com>
2025-10-01 23:23:28 +0900

    feat: Zen Maru Gothicフォントを追加し、全体のフォントスタイルを統一

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Zen Maru Gothic',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica',
+          'Arial',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+          'sans-serif',
+        ],
+      },
+    },
   },
   plugins: [],
 };
